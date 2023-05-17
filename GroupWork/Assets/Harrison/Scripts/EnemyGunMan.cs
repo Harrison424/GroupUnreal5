a51a703b774604bd5a367b92d4e77f59added463
RhysBrooke <98460415+RhysBrooke@users.noreply.github.com>
2023-05-17 11:25:58 +0100

    fuckit PT2

--- GroupWork/Assets/Harrison/Scripts/EnemyGunMan.cs
+++ GroupWork/Assets/Harrison/Scripts/EnemyGunMan.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public class EnemyGunMan : MonoBehaviour
 {
     public Transform target;
+    public float timeBtwShots;
+    public Transform eBullet;
+    public Transform eBulletSpawn;
+    Animator Anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Anim = this.transform.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         FaceTarget();
+        if (timeBtwShots <= 0)
+        {
+            Vector3 aimDir = (target.position - eBulletSpawn.position + new Vector3(0.0f, 1f, 0.0f)).normalized;
+            Instantiate(eBullet, eBulletSpawn.position, Quaternion.LookRotation(aimDir, Vector3.up));
+            timeBtwShots = 1;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
     }
     void FaceTarget()
     {
+        
         Vector3 direction = (target.position - transform.position).normalized;
-
+        Anim.SetBool("Aim", true);
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
